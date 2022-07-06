825a4c69a6e968c92c9bad03587ae825a7a1b868
Neko Of The Abyss <psi@innocent.com>
2022-07-06 21:30:51 +0530

    update bettermap version

--- deps/utils.ts
+++ deps/utils.ts
@@ -1,2 +1,2 @@
-export { BetterMap } from "https://deno.land/x/bettermap@v1.1.1/mod.ts";
+export { BetterMap } from "https://deno.land/x/bettermap@v1.2.1/mod.ts";
 export { importAll, importAllTextFiles } from "https://deno.land/x/importall@v1.1.1/mod.ts"
